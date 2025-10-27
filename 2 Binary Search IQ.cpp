d926a1e76b3eb9e9d8f5ce0ff8f49ae767b284fb
simran <simrankumaribodhgaya93@gmail.com>
2025-10-27 23:27:41 +0530

    peak element in a mountain array

--- 2 Binary Search IQ.cpp
+++ 2 Binary Search IQ.cpp
@@ -53,7 +53,7 @@ int main(){
     int even[6] = {2,4,6,7,89,7};
     int odd[5] = {45,6,53,1,5};
 
-    int evenIndex = firsOcc(even, 6, 7);
+    int evenIndex = firstOcc(even, 6, 7);
     int oddIndex = lastOcc(odd, 5, 53);
     cout<<"index of 7 is " << endl;
     cout<<"index of 53 is " << endl;
---
+++ 3 Binary Search IQ.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+
+int peakElement(int arr[], int n, int key){
+    
+    int s = 0;
+    int e = n - 1;
+
+    int mid =  (e + s) / 2;
+
+    while(s <= e){
+
+        if(arr[mid] == key){
+            return mid;
+        }else if(arr[mid] > key){
+            e = mid - 1;
+
+        }else {
+            s = mid + 1;
+        }
+        mid = (s + e) / 2;
+    }
+    return -1;
+}
+int main(){
+
+    int even[6] = {2,4,6,7,89,7};
+    int odd[5] = {45,6,53,1,5};
+
+    int evenIndex = binarySearch(even, 6, 7);
+    int oddIndex = binarySearch(odd, 5, 53);
+    cout<<"index of 7 is " << endl;
+    cout<<"index of 53 is " << endl;
+
+ 
+    return 0;
+}
